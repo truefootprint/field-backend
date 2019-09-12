d9deeb7881861f40edb5189603e8d8bf1ffd234b
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-12 13:27:47 +0100

    Add a ProjectQuestion#project method

--- app/models/project_question.rb
+++ app/models/project_question.rb
@@ -3,4 +3,13 @@ class ProjectQuestion < ApplicationRecord
   belongs_to :question
 
   validates :order, presence: true
+
+  def project
+    case subject_type
+    when "Project"
+      subject
+    when "ProjectActivity"
+      subject.project
+    end
+  end
 end
--- spec/models/project_question_spec.rb
+++ spec/models/project_question_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe ProjectQuestion do
       expect(project_question).to be_invalid
     end
   end
+
+  describe "#project" do
+    it "returns the subject when the question is about a project" do
+      project = FactoryBot.create(:project)
+      project_question = FactoryBot.create(:project_question, subject: project)
+
+      expect(project_question.project).to eq(project)
+    end
+
+    it "returns the activity's project when the question is about an activity" do
+      project_activity = FactoryBot.create(:project_activity)
+      project_question = FactoryBot.create(:project_question, subject: project_activity)
+
+      expect(project_question.project).to eq(project_activity.project)
+    end
+  end
 end
