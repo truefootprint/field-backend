b8eaab40db014ed6843722269691628a801b7f3f
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-19 16:56:25 +0100

    Add a feature test for answering questions

--- app/controllers/my_updates_controller.rb
+++ app/controllers/my_updates_controller.rb
@@ -1,5 +1,11 @@
 class MyUpdatesController < ApplicationController
   def create
+    params.fetch(:actions).each do |action|
+      id, value = action.values_at(:id, :value)
+
+      Response.create!(user: current_user, value: value, project_question_id: id)
+    end
+
     head :created
   end
 end
--- spec/features/answering_questions_spec.rb
+++ spec/features/answering_questions_spec.rb
@@ -1,20 +1,60 @@
 RSpec.describe "Answering questions" do
-  let(:project_question) { FactoryBot.create(:project_question) }
-
-  before do
-    FactoryBot.create(:user, name: "Test") # TODO: authentication
-    FactoryBot.create(:role, name: "Test")
-  end
+  let!(:user) { FactoryBot.create(:user, name: "Test") }
+  let!(:role) { FactoryBot.create(:role, name: "Test") }
 
   let(:auth) { { name: "Test", role: "Test" } }
 
+  let(:project) { FactoryBot.create(:project) }
+
+  let(:pa1) { FactoryBot.create(:project_activity, project: project) }
+  let(:pa2) { FactoryBot.create(:project_activity, project: project) }
+
+  let(:question) { FactoryBot.create(:question, text: "Is this activity finished?") }
+
+  let(:pq1) { FactoryBot.create(:project_question, subject: pa1) }
+  let(:pq2) { FactoryBot.create(:project_question, subject: pa1, question: question) }
+  let(:pq3) { FactoryBot.create(:project_question, subject: pa2) }
+  let(:pq4) { FactoryBot.create(:project_question, subject: pa2, question: question) }
+
+  before do
+    FactoryBot.create(:completion_question, question: question, completion_value: "yes")
+
+    [project, pa1, pa2, pq1, pq2, pq3, pq4].each do |subject|
+      FactoryBot.create(:visibility, subject: subject, visible_to: user)
+    end
+  end
+
+  def current_project_activity
+    parsed_json.fetch(:projects).first.fetch(:current_project_activity)
+  end
+
+  def completion_questions
+    parsed_json.fetch(:completion_questions)
+  end
+
+  def post_action(action)
+    post "/my_updates", auth.merge(actions: [action])
+  end
+
   scenario "creating responses to questions that have been answered" do
-    actions = [
-      { action: "AnswerQuestion", question_id: project_question.id, value: "yes" },
-    ]
+    get "/my_data", auth
+    expect(response.status).to eq(200)
+    expect(current_project_activity).to eq(id: pa1.id, name: pa1.activity.name)
+    expect(completion_questions).to eq [{
+      question_id: question.id, completion_value: "yes"
+    }]
 
-    post "/my_updates", { actions: actions }.merge(auth)
-
+    post_action(action: "AnswerQuestion", id: pq2.id, value: "yes")
     expect(response.status).to eq(201)
+
+    get "/my_data", auth
+    expect(response.status).to eq(200)
+    expect(current_project_activity).to eq(id: pa2.id, name: pa2.activity.name)
+
+    post_action(action: "AnswerQuestion", id: pq4.id, value: "yes")
+    expect(response.status).to eq(201)
+
+    get "/my_data", auth
+    expect(current_project_activity).to be_nil
   end
 end
