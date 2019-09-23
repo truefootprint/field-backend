83a47e3b35a6ec823b650881f2f752dc7044839d
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-23 10:29:20 +0100

    Remove PrototypesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "/1", to: "prototypes#topic_and_question_listing"
-  get "/2", to: "prototypes#mark_workshop_as_finished"
-
   resources :my_data, only: :index
   resources :my_updates, only: :create
 end
