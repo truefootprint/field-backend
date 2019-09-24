35ec38aedb0c79e0ebc0575eca1533156132c24b
Chris Patuzzo <chris@patuzzo.co.uk>
2019-09-24 12:17:50 +0100

    Add an Involvement.for_role method

--- app/models/involvement.rb
+++ app/models/involvement.rb
@@ -1,4 +1,20 @@
 class Involvement < ApplicationRecord
   belongs_to :project_activity
   belongs_to :user
+
+  scope :for_role, -> (role) do
+    user_roles = UserRole.where(role: role)
+
+    visibilities = Visibility.where(visible_to: user_roles)
+    projects = visibilities.subject_ids(Project)
+    project_activities = visibilities.subject_ids(ProjectActivity)
+
+    involvements = includes(project_activity: :project)
+      .where(user: user_roles.select(:user_id))
+
+    scope1 = involvements.where(project_activity_id: project_activities)
+    scope2 = involvements.where(project_activities: { project_id: projects })
+
+    scope1.or(scope2)
+  end
 end
--- spec/models/involvement_spec.rb
+++ spec/models/involvement_spec.rb
@@ -6,4 +6,46 @@ RSpec.describe Involvement do
       expect(involvement).to be_valid
     end
   end
+
+  describe ".for_role" do
+    it "returns involvements whose user has a user_role with visibility of the project activity" do
+      inv1, _inv2 = FactoryBot.create_list(:involvement, 2)
+      user_role = FactoryBot.create(:user_role, user: inv1.user)
+
+      FactoryBot.create(
+        :visibility,
+        subject: inv1.project_activity,
+        visible_to: user_role,
+      )
+
+      expect(Involvement.for_role(user_role.role)).to eq [inv1]
+    end
+
+    it "returns involvements whose visibility is at a project level instead of project activity" do
+      inv1, _inv2 = FactoryBot.create_list(:involvement, 2)
+      user_role = FactoryBot.create(:user_role, user: inv1.user)
+
+      FactoryBot.create(
+        :visibility,
+        subject: inv1.project_activity.project,
+        visible_to: user_role,
+      )
+
+      expect(Involvement.for_role(user_role.role)).to eq [inv1]
+    end
+
+    it "does not returns involvements for other users" do
+      involvement = FactoryBot.create(:involvement)
+      another_user = FactoryBot.create(:user)
+      user_role = FactoryBot.create(:user_role, user: another_user)
+
+      FactoryBot.create(
+        :visibility,
+        subject: involvement.project_activity,
+        visible_to: user_role,
+      )
+
+      expect(Involvement.for_role(user_role.role)).to be_empty
+    end
+  end
 end
