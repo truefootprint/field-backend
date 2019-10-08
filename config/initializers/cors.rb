3d096c74c7e0546f0d97809f2cf806b80293a30e
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-08 14:39:39 +0100

    Allow #update and #destroy actions via the API

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "*"
     resource "*",
       headers: :any,
-      methods: [:get, :post, :options],
+      methods: [:get, :post, :put, :delete, :options],
       expose: "X-Total-Count"
   end
 end
