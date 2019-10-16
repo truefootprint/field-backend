9ec64d639acaecf05ba9c2a62feb298eee32bb49
Chris Patuzzo <chris@patuzzo.co.uk>
2019-10-16 14:20:01 +0100

    Ensure Unit types are consistent with Unitwise properties

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,5 +1,9 @@
 class Unit < ApplicationRecord
-  TYPES = %w[length volume weight].freeze
+  TYPES = %w[fraction length mass volume temperature time].freeze
+
+  def self.available_types
+    Unitwise::Atom.all.map(&:property).uniq
+  end
 
   self.inheritance_column = :_disabled
 
@@ -7,9 +11,29 @@ class Unit < ApplicationRecord
   validates :type, inclusion: { in: TYPES }
 
   validate :name_is_recognised
+  validate :type_matches_property
 
   def name_is_recognised
-    return if Unitwise.search(name).present?
+    return if errors.present? || unitwise_unit
     errors.add(:name, "must be a recognised unit (lower-case, singular)")
+  end
+
+  def type_matches_property
+    return if errors.present? || type == unitwise_property
+    errors.add(:type, "is #{type} but #{name} is a #{unitwise_property}")
+  end
+
+  private
+
+  def unitwise_unit
+    @unitwise_unit ||= Unitwise.search(name).first if name.present?
+  end
+
+  def unitwise_atom
+    unitwise_unit.atoms.first if unitwise_unit
+  end
+
+  def unitwise_property
+    unitwise_atom.property if unitwise_atom
   end
 end
--- spec/models/unit_spec.rb
+++ spec/models/unit_spec.rb
@@ -27,5 +27,25 @@ RSpec.describe Unit do
       unit.type = "unknown"
       expect(unit).to be_invalid
     end
+
+    it "requires a type that matches the Unitwise property" do
+      unit.name = "meter"
+      unit.type = "weight"
+
+      expect(unit).to be_invalid
+    end
+  end
+
+  describe ".available_types" do
+    it "returns all Unitwise properties" do
+      types = Unit.available_types
+
+      expect(types.size).to be >= 100
+      expect(types).to include("force", "pressure")
+    end
+
+    it "sets TYPES to a subset of the available types" do
+      expect(Unit::TYPES - Unit.available_types).to be_empty
+    end
   end
 end
