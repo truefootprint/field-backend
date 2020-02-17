e1b5375eb189f45f452c36116265af2cf867e8f1
Chris Patuzzo <chris@patuzzo.co.uk>
2020-02-17 11:38:53 +0000

    Try to mitigate consistency problems with ancestors/descendants

--- app/models/versioned_content.rb
+++ app/models/versioned_content.rb
@@ -1,12 +1,29 @@
 class VersionedContent < ApplicationRecord
-  has_ancestry
+  SUBJECT_TYPES = %w[Issue Resolution].freeze
 
   belongs_to :subject, polymorphic: true
   belongs_to :user
 
+  has_ancestry
+
   validates :content, presence: true
+  validates :subject_type, inclusion: { in: SUBJECT_TYPES }
+
+  validate :subject do
+    subject_is_the_same_as(ancestors, "ancestors")
+    subject_is_the_same_as(descendants, "descendents") unless new_record?
+  end
 
   after_initialize do
     self.photos = "[]" if photos.blank?
+  end
+
+  private
+
+  def subject_is_the_same_as(scope, name)
+    subjects = scope.pluck(:subject_type, :subject_id)
+    return if subjects.all? { |s| s == [subject_type, subject_id] }
+
+    errors.add(:subject, "must be the same as its #{name}")
   end
 end
--- spec/models/versioned_content_spec.rb
+++ spec/models/versioned_content_spec.rb
@@ -10,6 +10,29 @@ RSpec.describe VersionedContent do
       versioned_content.content = " "
       expect(versioned_content).to be_invalid
     end
+
+    it "requires a known subject type" do
+      versioned_content.subject_type = "unknown"
+      expect(versioned_content).to be_invalid
+    end
+
+    it "requires the subject is set to the same as its ancestors" do
+      parent = FactoryBot.create(:versioned_content)
+
+      versioned_content.parent = parent
+      versioned_content.subject = FactoryBot.create(:issue)
+
+      expect(versioned_content).to be_invalid
+    end
+
+    it "requires the subject is set to the same as its descendants" do
+      parent = FactoryBot.create(:versioned_content)
+      child = FactoryBot.create(:versioned_content, parent: parent, subject: parent.subject)
+
+      parent.subject = FactoryBot.create(:issue)
+
+      expect(parent).to be_invalid
+    end
   end
 
   it "defaults 'photos' to an empty json array" do
