cd3604a9df6f8393159e2a6574aaa4832c424e8e
Chris Patuzzo <chris@patuzzo.co.uk>
2020-02-17 13:46:15 +0000

    Remove ‘description’ from Issue and Resolution

--- db/migrate/20190925140209_create_issues.rb
+++ db/migrate/20190925140209_create_issues.rb
@@ -3,7 +3,6 @@ class CreateIssues < ActiveRecord::Migration[6.0]
     create_table :issues do |t|
       t.belongs_to :subject, polymorphic: true
       t.belongs_to :user
-      t.text :description, null: false
       t.boolean :critical, null: false, default: false
       t.timestamps
     end
