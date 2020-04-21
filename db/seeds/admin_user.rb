dbd0c197e6920de386ce7063699421919dce3281
Chris Patuzzo <chris@patuzzo.co.uk>
2020-04-21 12:15:19 +0100

    Fix the admin setup by grouping into their own programme

--- db/seeds/admin_user.rb
+++ db/seeds/admin_user.rb
@@ -1,5 +1,17 @@
 user = User.create!(name: "admin", country_code: "+44", phone_number: "1234567890")
-role = Role.create!(name: "admin")
-
-ProjectRole.create!(project: Project.first, role: role, order: 1)
 ApiToken.generate_for!(user)
+
+programme = Programme.create!(
+  name: "Admin programme",
+  description: <<~TEXT.squish
+   A programme to group together all admins of the FieldApp. Eventually, we'll
+   have admins per-project but for now, every admin can administer every project.
+  TEXT
+)
+
+project_type = ProjectType.create!(name: "Admin project type")
+project = Project.create!(name: "Admin project", programme: programme, project_type: project_type)
+role = Role.create!(name: "admin")
+project_role = ProjectRole.create!(project: project, role: role, order: 1)
+
+Registration.create!(user: user, project_role: project_role)
